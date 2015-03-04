23f82d5a55f9467da95a5c9eb884f9078f94f37e
drzlab <drzlab@hotmail.com>
2015-03-04 17:17:22 +0800

    add single test

---
+++ test/single_request/single_test.py
@@ -0,0 +1,16 @@
+# -*-coding:utf8 -*-
+from labor import Labor
+import time
+import zmq
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        addr = sys.argv[1]
+    else:
+        addr = '*:1808'
+    lb = Labor(addr, Labor.TYPE_PUBSUB)
+    time.sleep(2)
+    lb.use('__echo', what='single test!!!!')
+    time.sleep(10)
+    
