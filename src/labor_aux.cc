3648f45eedf63ad677d307caa7b20fbf6ce9f5d6
drzunny <drzunny@hotmail.com>
2015-05-03 13:58:36 +0800

    add extension api

--- src/labor_aux.cc
+++ src/labor_aux.cc
@@ -1,1 +1,66 @@
 #include "labor_aux.h"
+#include "labor_log.h"
+#include <unordered_map>
+
+using namespace std;
+
+
+/* -------------------------------------
+*  Helper functions and properties
+* -------------------------------------*/
+static unordered_map<string, void *> s_service_pool;
+
+
+/* -------------------------------------
+ *  The Extension API definition
+ * -------------------------------------*/
+string
+labor::ext_json_encode(const void * jsonptr)  {
+    return "";
+}
+
+
+void
+labor::ext_json_decode(const char * str, void ** jsonptr)   {
+
+}
+
+
+void
+labor::ext_service_init(int type)   {
+
+}
+
+
+void
+labor::ext_service_publish(void * hnd, const char * message)    {
+
+}
+
+
+void
+labor::ext_service_push(void * hnd, const char * message)   {
+
+}
+
+
+void
+labor::ext_service_pull(void * hnd)   {
+
+}
+
+
+void
+labor::ext_logger_debug(const char * message)   { LOG_DEBUG(message); }
+
+
+void
+labor::ext_logger_info(const char * message)    { LOG_INFO(message); }
+
+
+void 
+labor::ext_logger_warning(const char * message) { LOG_WARNING(message); }
+
+
+void
+labor::ext_logger_error(const char * message)   { LOG_ERROR(message); }
--- src/labor_aux.h
+++ src/labor_aux.h
@@ -1,5 +1,32 @@
 #ifndef __LABOR_VM_AUX_H__
 #define __LABOR_VM_AUX_H__
 
+#include <string>
+
+namespace labor
+{
+    enum EServiceType
+    {
+        SV_PUBLISH,
+        SV_PUSH,
+        SV_PULL
+    };
+
+    // Json library
+    std::string ext_json_encode(const void * jsonptr);
+    void ext_json_decode(const char *str, void ** jsonptr);
+
+    // Push service
+    void ext_service_init(int type);
+    void ext_service_push(void * hnd, const char * message);
+    void ext_service_pull(void * hnd);
+    void ext_service_publish(void * hnd, const char * message);
+
+    // logger library
+    void ext_logger_debug(const char * message);
+    void ext_logger_info(const char * message);
+    void ext_logger_warning(const char * message);
+    void ext_logger_error(const char* message);
+}
 
 #endif
